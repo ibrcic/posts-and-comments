3d99f3d7ac7c3401fda376e34c0ac4e1f11f1b6f
Ivan Brcic <ivan@enum.hr>
2022-03-06 22:47:31 +0100

    Added post details and filter tests

--- src/app/post-details/post-details.component.spec.ts
+++ src/app/post-details/post-details.component.spec.ts
@@ -1,4 +1,8 @@
+import { HttpClientTestingModule } from '@angular/common/http/testing';
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { Router } from '@angular/router';
+import { RouterTestingModule } from '@angular/router/testing';
+import { API_BASE_URL } from '../app.module';
 
 import { PostDetailsComponent } from './post-details.component';
 
@@ -6,20 +10,28 @@ describe('PostDetailsComponent', () => {
   let component: PostDetailsComponent;
   let fixture: ComponentFixture<PostDetailsComponent>;
 
-  // beforeEach(async () => {
-  //   await TestBed.configureTestingModule({
-  //     declarations: [ PostDetailsComponent ]
-  //   })
-  //   .compileComponents();
-  // });
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      imports: [
+        RouterTestingModule,
+        HttpClientTestingModule
+      ],
+      declarations: [PostDetailsComponent],
+      providers: [
+        { provide: API_BASE_URL, useValue: '' }
+      ]
+    })
+      .compileComponents();
+  });
 
-  // beforeEach(() => {
-  //   fixture = TestBed.createComponent(PostDetailsComponent);
-  //   component = fixture.componentInstance;
-  //   fixture.detectChanges();
-  // });
+  beforeEach(() => {
+    fixture = TestBed.createComponent(PostDetailsComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
 
-  // it('should create', () => {
-  //   expect(component).toBeTruthy();
-  // });
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+
 });
--- src/app/posts/components/posts-filter/posts-filter.component.spec.ts
+++ src/app/posts/components/posts-filter/posts-filter.component.spec.ts
@@ -2,6 +2,7 @@ import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { PostsFilterComponent } from './posts-filter.component';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { FilterOptions } from './filter-options';
 
 describe('PostsFilterComponent', () => {
   let component: PostsFilterComponent;
@@ -24,4 +25,37 @@ describe('PostsFilterComponent', () => {
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+
+  it('should update the form with writeValue', () => {
+    const usersNameQuery = 'query'
+    component.writeValue(new FilterOptions({usersNameQuery}))
+    expect(component.form.value.usersNameQuery).toBe(usersNameQuery);
+  });
+
+  it('registerOnChange should set the onChange function', () => {
+    const func = () => {};
+    component.registerOnChange(func);
+    expect(component.onChange).toBe(func);
+  });
+
+  it('registerOnTouched should set the onTouched function', () => {
+    const func = () => {};
+    component.registerOnTouched(func);
+    expect(component.onTouched).toBe(func);
+  });
+
+  it('should call onChange on form value change', () => {
+    component.ngOnInit();
+    const outputSpy = spyOn(component, 'onChange').and.callThrough();
+    component.writeValue(new FilterOptions())
+    expect(outputSpy).toHaveBeenCalled();
+  });
+
+  it('should emit to filterOptionsChange on form value change', () => {
+    component.ngOnInit();
+    const outputSpy = spyOn(component.filterOptionsChange, 'next').and.callThrough();
+    component.writeValue(new FilterOptions())
+    expect(outputSpy).toHaveBeenCalled();
+  });
+
 });
